d7bfff0f39823b2be8dc829d65379d2235cb86bb
K Hariram <harirarn@gmail.com>
2013-07-24 13:40:44 +0530

    Create english_lower.py
    only 'a' for now

---
+++ english_lower.py
@@ -0,0 +1,11 @@
+letter a
+  reposition x f 90.0 3.0 y f 80.0 4.0 angle a 3.16 0.02
+  stroke steps 30 1 len 1.0 0.1 turn -0.0006 -0.0007 0.001
+  arc radius -50 turn 1.5708 2 0.001 len 1.0 0.1
+  arc radius -30 turn 1.5708 1 0.001 len 1.0 0.1
+  arc radius -47 turn 1.5708 3 0.002 len 1.0 0.1
+  stroke steps 30 1 len 1.0 0.1 turn -0.001 -0.005 0.002
+  stroke steps 10 0 len 1.0 0.1 turn -0.3142 -0.3142 0.002
+  stroke steps 80 5 len 1.0 0.2 turn -0.006 -0.012 0.002
+  reposition x f 100.0 4.0 y f 0.0 1.0 angle a 0.0 0.0
+end
